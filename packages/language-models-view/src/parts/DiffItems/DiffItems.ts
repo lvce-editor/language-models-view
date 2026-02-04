f511fb963953c139ffe16d4debc733eb1d9f9a10
Le Vivilet <levivilet@pm.me>
2026-02-04 21:51:06 +0100

    diff

--- packages/language-models-view/src/parts/DiffItems/DiffItems.ts
+++ packages/language-models-view/src/parts/DiffItems/DiffItems.ts
@@ -1,5 +1,5 @@
 import type { LanguageModelsState } from '../LanguageModelsState/LanguageModelsState.ts'
 
 export const isEqual = (oldState: LanguageModelsState, newState: LanguageModelsState): boolean => {
-  return oldState.models === newState.models
+  return oldState.models === newState.models && oldState.filteredModels === newState.filteredModels
 }
