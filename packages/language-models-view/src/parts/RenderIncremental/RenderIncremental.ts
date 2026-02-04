1cd72be56b986d8d4f59692e7d41b50bb9cf1204
Le Vivilet <levivilet@pm.me>
2026-02-04 20:26:33 +0100

    fix

--- packages/language-models-view/src/parts/RenderIncremental/RenderIncremental.ts
+++ packages/language-models-view/src/parts/RenderIncremental/RenderIncremental.ts
@@ -7,6 +7,5 @@ export const renderIncremental = (oldState: LanguageModelsState, newState: Langu
   const oldDom = renderItems(oldState, oldState)[2]
   const newDom = renderItems(newState, newState)[2]
   const patches = diffTree(oldDom, newDom)
-  console.log({ patches })
   return [ViewletCommand.SetPatches, newState.uid, patches]
 }
