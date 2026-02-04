a1dc2879e200a9346bcf62f6bd2139633969f343
Le Vivilet <levivilet@pm.me>
2026-02-04 18:30:33 +0100

    unused

--- packages/language-models-view/src/parts/HandleBlur/HandleBlur.ts
+++
@@ -1,8 +0,0 @@
-import type { LanguageModelsState } from '../LanguageModelsState/LanguageModelsState.ts'
-
-export const handleBlur = (state: LanguageModelsState): LanguageModelsState => {
-  return {
-    ...state,
-    focused: false,
-  }
-}
--- packages/language-models-view/src/parts/HandleClick/HandleClick.ts
+++
@@ -1,9 +0,0 @@
-import type { LanguageModelsState } from '../LanguageModelsState/LanguageModelsState.ts'
-import { getIndexFromPosition } from '../GetIndexFromPosition/GetIndexFromPosition.ts'
-import { handleClickIndex } from '../HandleClickIndex/HandleClickIndex.ts'
-
-export const handleClick = async (state: LanguageModelsState, button: number, eventX: number, eventY: number): Promise<LanguageModelsState> => {
-  const { filteredItems, height, itemHeight, y } = state
-  const index = getIndexFromPosition(y, eventX, eventY, itemHeight, filteredItems.length, height)
-  return handleClickIndex(state, button, index, eventX, eventY)
-}
