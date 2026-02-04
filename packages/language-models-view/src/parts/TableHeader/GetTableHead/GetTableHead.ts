5b69b5fa0f64c963cea292f73b83bc38770dd8e9
Le Vivilet <levivilet@pm.me>
2026-02-04 20:25:54 +0100

    fix

--- packages/language-models-view/src/parts/TableHeader/GetTableHead/GetTableHead.ts
+++ packages/language-models-view/src/parts/TableHeader/GetTableHead/GetTableHead.ts
@@ -1,17 +1,9 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
-<<<<<<< HEAD
 import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
-=======
-import { VirtualDomElements } from '@lvce-editor/constants'
->>>>>>> origin/main
 
 export const getTableHead = (): VirtualDomNode => {
   return {
     childCount: 1,
-<<<<<<< HEAD
-    type: VirtualDomElements.Thead,
-=======
     type: VirtualDomElements.THead,
->>>>>>> origin/main
   }
 }
