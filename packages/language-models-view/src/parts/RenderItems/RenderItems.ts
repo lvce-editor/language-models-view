5dad58215588452ad606fb316ae82e1e7234b083
Le Vivilet <levivilet@pm.me>
2026-02-04 18:31:07 +0100

    fix

--- packages/language-models-view/src/parts/RenderItems/RenderItems.ts
+++ packages/language-models-view/src/parts/RenderItems/RenderItems.ts
@@ -1,12 +1,6 @@
 import { ViewletCommand } from '@lvce-editor/constants'
 import type { LanguageModelsState } from '../LanguageModelsState/LanguageModelsState.ts'
-import { getActivityBarVirtualDom } from '../GetActivityBarVirtualDom/GetActivityBarVirtualDom.ts'
 
 export const renderItems = (oldState: LanguageModelsState, newState: LanguageModelsState): any => {
-  const { filteredItems, initial, uid } = newState
-  if (initial) {
-    return [ViewletCommand.SetDom2, uid, []]
-  }
-  const dom = getActivityBarVirtualDom(filteredItems)
-  return [ViewletCommand.SetDom2, uid, dom]
+  return [ViewletCommand.SetDom2, newState.uid, []]
 }
