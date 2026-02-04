e878e6b899cd00b879406e75a32b32d45640ba54
Le Vivilet <levivilet@pm.me>
2026-02-04 20:08:02 +0100

    feature: refactor language models virtual DOM structure and add filter input

--- packages/language-models-view/src/parts/GetLanguageModelsVirtualDom/GetLanguageModelsVirtualDom.ts
+++ packages/language-models-view/src/parts/GetLanguageModelsVirtualDom/GetLanguageModelsVirtualDom.ts
@@ -1,61 +1,14 @@
 import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
 import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { LanguageModel } from '../LanguageModel/LanguageModel.ts'
-
-const getTableHeaderRow = (): VirtualDomNode => {
-  return {
-    childCount: 2,
-    type: VirtualDomElements.Tr,
-  }
-}
-
-const getTableHeaderCell = (text: string): VirtualDomNode => {
-  return {
-    childCount: 1,
-    type: VirtualDomElements.Th,
-  }
-}
-
-const getTableRow = (model: LanguageModel): VirtualDomNode => {
-  return {
-    childCount: 2,
-    type: VirtualDomElements.Tr,
-  }
-}
-
-const getTableCell = (text: string): VirtualDomNode => {
-  return {
-    childCount: 1,
-    type: VirtualDomElements.Td,
-  }
-}
-
-const getTableHead = (): VirtualDomNode => {
-  return {
-    childCount: 1,
-    type: VirtualDomElements.Thead,
-  }
-}
-
-const getTableBody = (models: readonly LanguageModel[]): VirtualDomNode => {
-  return {
-    childCount: models.length,
-    type: VirtualDomElements.Tbody,
-  }
-}
+import { getFilterInput } from '../FilterInput/GetFilterInputVirtualDom.ts'
+import { getTableBodyVirtualDom } from '../TableBody/GetTableBodyVirtualDom.ts'
+import { getTableHeaderVirtualDom } from '../TableHeader/GetTableHeaderVirtualDom.ts'
 
 const getTable = (models: readonly LanguageModel[]): VirtualDomNode => {
   return {
     childCount: 2,
     type: VirtualDomElements.Table,
-  }
-}
-
-const getFilterInput = (): VirtualDomNode => {
-  return {
-    className: 'LanguageModelsFilter',
-    placeholder: 'Filter language models...',
-    type: VirtualDomElements.Input,
   }
 }
 
@@ -68,31 +21,7 @@ export const getLanguageModelsVirtualDom = (models: readonly LanguageModel[]): r
     },
     getFilterInput(),
     getTable(models),
-    getTableHead(),
-    getTableHeaderRow(),
-    getTableHeaderCell('ID'),
-    {
-      text: 'ID',
-      type: VirtualDomElements.Text,
-    },
-    getTableHeaderCell('Name'),
-    {
-      text: 'Name',
-      type: VirtualDomElements.Text,
-    },
-    getTableBody(models),
-    ...models.flatMap((model) => [
-      getTableRow(model),
-      getTableCell(model.id),
-      {
-        text: model.id,
-        type: VirtualDomElements.Text,
-      },
-      getTableCell(model.name),
-      {
-        text: model.name,
-        type: VirtualDomElements.Text,
-      },
-    ]),
+    ...getTableHeaderVirtualDom(),
+    ...getTableBodyVirtualDom(models),
   ]
 }
---
+++ packages/language-models-view/src/parts/TableBody/GetTableBodyVirtualDom.ts
@@ -0,0 +1,43 @@
+import type { VirtualDomNode } from '@lvce-editor/virtual-dom-worker'
+import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
+import type { LanguageModel } from '../LanguageModel/LanguageModel.ts'
+
+const getTableRow = (model: LanguageModel): VirtualDomNode => {
+  return {
+    childCount: 2,
+    type: VirtualDomElements.Tr,
+  }
+}
+
+const getTableCell = (text: string): VirtualDomNode => {
+  return {
+    childCount: 1,
+    type: VirtualDomElements.Td,
+  }
+}
+
+export const getTableBody = (models: readonly LanguageModel[]): VirtualDomNode => {
+  return {
+    childCount: models.length,
+    type: VirtualDomElements.Tbody,
+  }
+}
+
+export const getTableBodyVirtualDom = (models: readonly LanguageModel[]): readonly VirtualDomNode[] => {
+  return [
+    getTableBody(models),
+    ...models.flatMap((model) => [
+      getTableRow(model),
+      getTableCell(model.id),
+      {
+        text: model.id,
+        type: VirtualDomElements.Text,
+      },
+      getTableCell(model.name),
+      {
+        text: model.name,
+        type: VirtualDomElements.Text,
+      },
+    ]),
+  ]
+}
