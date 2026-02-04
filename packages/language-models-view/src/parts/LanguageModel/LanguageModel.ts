94b4d4e6b0aef12eac7ed7e70b44ded4929d66c4
Le Vivilet <levivilet@pm.me>
2026-02-04 22:05:46 +0100

    fix

--- packages/language-models-view/src/parts/LanguageModel/LanguageModel.ts
+++ packages/language-models-view/src/parts/LanguageModel/LanguageModel.ts
@@ -1,4 +1,7 @@
+import type { ModelCapabilities } from '../ModelCapabilities/ModelCapabilities.ts'
+
 export interface LanguageModel {
+  readonly capabilities: ModelCapabilities
   readonly enabled: boolean
   readonly id: string
   readonly inputContextSize: number
