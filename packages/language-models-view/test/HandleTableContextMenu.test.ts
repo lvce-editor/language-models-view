380014b1b27563438eef23cb710e7a07534b6d21
Le Vivilet <levivilet@pm.me>
2026-02-05 13:18:51 +0100

    fix

--- packages/language-models-view/test/HandleTableContextMenu.test.ts
+++ packages/language-models-view/test/HandleTableContextMenu.test.ts
@@ -125,8 +125,8 @@ test('handleTableContextMenu handles header click by not showing context menu', 
 
   const state: LanguageModelsState = {
     ...createDefaultState(),
-    filteredModels: models,
     uid: 55,
+    filteredModels: models,
   }
 
   const result = await handleTableContextMenu(state, 0, 0)
@@ -154,8 +154,8 @@ test('handleTableContextMenu handles large coordinates', async () => {
 
   const state: LanguageModelsState = {
     ...createDefaultState(),
-    filteredModels: models,
     uid: 77,
+    filteredModels: models,
   }
 
   await handleTableContextMenu(state, 1920, 1080)
@@ -182,8 +182,8 @@ test('handleTableContextMenu uses correct menu ID', async () => {
 
   const state: LanguageModelsState = {
     ...createDefaultState(),
-    filteredModels: models,
     uid: 123,
+    filteredModels: models,
   }
 
   await handleTableContextMenu(state, 250, 350)
